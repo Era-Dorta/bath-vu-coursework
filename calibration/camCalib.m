0836088b94d8a978c8f8174111ea654f28a888f5
Garoe <garoedp@gmail.com>
2015-01-10 17:18:02 +0000

    Adde 3d reconstruction code

--- calibration/camCalib.m
+++ calibration/camCalib.m
@@ -1,19 +1,26 @@
 close all;
 clear all;
 
-%figure; imshow(imread('images/cubeInstructions.png'));
-img = imreadgrey('images/cube_left.png');
+%img = imreadgrey('images/cube_left.png');
+img = imreadgrey('images/cube_right.png');
 figure; imshow(img); hold on;
 
 useSavedPoints = false;
 
 if useSavedPoints
-    input_p =  [325.5883,  402.1961,  1;
+    input_p_left =  [325.5883,  402.1961,  1;
         258.8746,  279.5106,    1;
         249.8286,   92.9382,    1;
         446.5777,   69.1926,    1;
         573.7862,  141.5601,    1;
         534.7756,  350.1820,    1];
+    
+    input_p =    [315.4117,  402.7615,    1;
+  105.6590,  350.1820,    1;
+   67.2138,  142.1254,    1;
+  193.8569,   69.1926,    1;
+  391.7367,   91.8074,    1;
+  382.6908,  277.2491,    1]
 else
     % Get six image calibration points from the user
     [x,y] = ginput(6);
@@ -21,9 +28,14 @@ else
 end
 
 % Predetermined 3d world position of the calibration points
-base_p = [-2, -2, 0, 1; -2, 2, 0, 1; ...
+base_p_left = [-2, -2, 0, 1; -2, 2, 0, 1; ...
     -2, 2, 4, 1; 2, 2, 4, 1; ...
     2, -2, 4, 1; 2, -2, 0, 1];
+
+% Right points
+base_p = [2, -2, 0, 1; -2, -2, 0, 1; ...
+    -2, -2, 4, 1; -2, 2, 4, 1; ...
+    2, 2, 4, 1; 2, 2, 0, 1];
 
 % Construct A matrix from both points
 A = makeA3dMatrix(base_p, input_p);
---
+++ stereo recontruction/stereoRecons.m
@@ -0,0 +1,41 @@
+close all;
+clear all;
+
+load('leftCam.mat');
+load('rightCam.mat');
+
+simpleRecons = false;
+
+if simpleRecons
+    img1 = imreadgrey('images/cube_left.png');
+    img2 = imreadgrey('images/cube_right.png');
+    
+    x_right = [217.5, 90.5; 268, 96.5; 321, 102.5; 190.5, 107.5; 243.5, 114.5; ...
+        299.5, 121.5; 159, 127.5; 215.5, 135.5; 275, 144; ...
+        132, 211; 189.5, 221.5; 250.5, 232.5; 140, 266.5; 195.5, 277.5; ...
+        253.5, 289.5; 148.5, 316.5; 201, 329; 256.5, 341.5]';
+    
+    x_left = [322.5, 102.5; 374, 97; 425, 90.5; 341.5, 122; 399, 114.5; ...
+        451, 108; 367, 144.5; 425, 136; 484.5, 127.5; ...
+        391.5, 232.5; 451.5, 221.5; 509, 211; 387.5, 289.5; 445.5, 277.5; ...
+        500.5, 265.5; 384.5, 341.5; 440, 329; 493, 316]';
+else
+    img1 = imreadgrey('images/cube_left.png');
+    img2 = imreadgrey('images/cube_right.png');
+
+    points1 = detectSURFFeatures(img1);
+    [features1, validPoints1] = extractFeatures(img1, points1);
+    
+    points2 = detectSURFFeatures(img2);
+    [features2, validPoints2] = extractFeatures(img2, points2);
+    
+    indexPairs = matchFeatures(features1,features2);
+    numMatch = size(indexPairs, 1);
+    x_left = validPoints1.Location(indexPairs(:, 1),:)';
+    x_right = validPoints2.Location(indexPairs(:, 2),:)';
+end
+
+X = Reconstruct(rightP, leftP, x_right, x_left);
+
+figure;
+plot3(X(1,:), X(2,:), X(3,:), 'r*');
