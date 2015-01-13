a10210f398abe3f63d3ef01e44bd9940c05b9be6
Garoe <garoedp@gmail.com>
2015-01-13 09:16:19 +0000

    ConvolutionTest now averages 5 execution times

--- convolution/convTest.m
+++ convolution/convTest.m
@@ -1,16 +1,27 @@
 close all;
 clear all;
 
-img1 = imreadgrey('lena.jpg');
-k = 1/9 * ones(3,3);
+img1 = imreadgrey('images/lena.jpg');
+%k = 1/9 * ones(3,3);
+k = [-1, 1];
 
 t1 = cputime;
-outI1 = conv2(img1, k, 'valid');
+    outI1 = conv2(img1, k, 'valid');
+    outI1 = conv2(img1, k, 'valid');
+    outI1 = conv2(img1, k, 'valid');
+    outI1 = conv2(img1, k, 'valid');
+    outI1 = conv2(img1, k, 'valid');
 t1 = cputime - t1;
+t1 = t1 / 5;
 
 t2 = cputime;
-outI2 = myconv(img1, k);
+    outI2 = myconv(img1, k);
+    outI2 = myconv(img1, k);
+    outI2 = myconv(img1, k);
+    outI2 = myconv(img1, k);
+    outI2 = myconv(img1, k);
 t2 = cputime - t2;
+t2 = t2 / 5;
 
 figure; imshow(outI1);
 figure; imshow(outI2);
@@ -19,8 +30,13 @@ error = sumsqr(outI1 - outI2);
 fprintf( 'Error in myconv is: %f \n', error );
 
 t3 = cputime;
-out3 = myconvFF(img1, k);
+    out3 = myconvFF(img1, k);
+    out3 = myconvFF(img1, k);
+    out3 = myconvFF(img1, k);
+    out3 = myconvFF(img1, k);
+    out3 = myconvFF(img1, k);
 t3 = cputime - t3;
+t3 = t3 / 5;
 
 figure; imshow(out3);
 
